5d466c327ebd78168f69ebca4920dc79e72ed941
Durica.Adrian <Durica@microcomp.sk>
2022-12-29 10:25:27 +0100

    1.0.1

--- Controllers/UpdateController.cs
+++ Controllers/UpdateController.cs
@@ -11,53 +11,64 @@ namespace elec.Controllers
     {
         public UpdateController(
         elec.Logging.ILogger<UpdateController> logger,
-        IControllerExceptionHandler exceptionHandler) 
+        IControllerExceptionHandler exceptionHandler)
         : base(logger, exceptionHandler)
-    {
-    }
+        {
+        }
         public IActionResult Index()
         {
-            if (HybridSupport.IsElectronActive)
+            try
             {
-                Electron.AutoUpdater.OnError += (message) => Electron.Dialog.ShowErrorBox("Error", message);
-                Electron.AutoUpdater.OnCheckingForUpdate += async () => await Electron.Dialog.ShowMessageBoxAsync("Checking for Update");
-                Electron.AutoUpdater.OnUpdateNotAvailable += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update not available");
-                Electron.AutoUpdater.OnUpdateAvailable += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update available" + info.Version);
-                Electron.AutoUpdater.OnDownloadProgress += (info) =>
+                if (HybridSupport.IsElectronActive)
                 {
-                    var message1 = "Download speed: " + info.BytesPerSecond + "\n<br/>";
-                    var message2 = "Downloaded " + info.Percent + "%" + "\n<br/>";
-                    var message3 = $"({info.Transferred}/{info.Total})" + "\n<br/>";
-                    var message4 = "Progress: " + info.Progress + "\n<br/>";
-                    var information = message1 + message2 + message3 + message4;
+                    Electron.AutoUpdater.OnError += (message) => Electron.Dialog.ShowErrorBox("Error", message);
+                    Electron.AutoUpdater.OnCheckingForUpdate += async () => await Electron.Dialog.ShowMessageBoxAsync("Checking for Update");
+                    Electron.AutoUpdater.OnUpdateNotAvailable += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update not available");
+                    Electron.AutoUpdater.OnUpdateAvailable += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update available" + info.Version);
+                    Electron.AutoUpdater.OnDownloadProgress += (info) =>
+                    {
+                        var message1 = "Download speed: " + info.BytesPerSecond + "\n<br/>";
+                        var message2 = "Downloaded " + info.Percent + "%" + "\n<br/>";
+                        var message3 = $"({info.Transferred}/{info.Total})" + "\n<br/>";
+                        var message4 = "Progress: " + info.Progress + "\n<br/>";
+                        var information = message1 + message2 + message3 + message4;
 
-                    var mainWindow = Electron.WindowManager.BrowserWindows.First();
-                    Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
-                };
-                Electron.AutoUpdater.OnUpdateDownloaded += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update complete!" + info.Version);
+                        var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                        Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
+                    };
+                    Electron.AutoUpdater.OnUpdateDownloaded += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update complete!" + info.Version);
 
-                Electron.IpcMain.On("auto-update", async (args) =>
-                {
-                    // Electron.NET CLI Command for deploy:
-                    // electronize build /target win /electron-params --publish=always
-          
-                    var currentVersion = await Electron.App.GetVersionAsync();
+                    Electron.IpcMain.On("auto-update", async (args) =>
+                    {
+                        // Electron.NET CLI Command for deploy:
+                        // electronize build /target win /electron-params --publish=always
 
-                    /* #if DEBUG */
-                    var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAsync();
-                    /* #else
-                    var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAndNotifyAsync();
-                    #endif */
+                        var currentVersion = await Electron.App.GetVersionAsync();
+                        _logger.Info($"Electron app current version: {currentVersion}");
 
-                    var availableVersion = updateCheckResult.UpdateInfo.Version;
-                    string information = $"Current version: {currentVersion} - available version: {availableVersion}";
+                        #if DEBUG
+                        var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAsync();
+                        #else
+                        var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAndNotifyAsync();
+                        #endif
 
-                    var mainWindow = Electron.WindowManager.BrowserWindows.First();
-                    Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
-                });
+                        var availableVersion = updateCheckResult.UpdateInfo.Version;
+                        _logger.Info($"Available version: {availableVersion}");
+
+                        string information = $"Current version: {currentVersion} - available version: {availableVersion}";
+                        _logger.Info(information);
+
+                        var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                        Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
+                    });
+                }
+
+                return View();
             }
-
-            return View();
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
     }
 }
