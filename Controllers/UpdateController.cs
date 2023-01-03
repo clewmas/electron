959eb3febf1a42040334e0a99cee7a0453cd0a33
Durica.Adrian <Durica@microcomp.sk>
2023-01-03 13:11:53 +0100

    auto update custom msgbox tst

--- Controllers/UpdateController.cs
+++ Controllers/UpdateController.cs
@@ -18,8 +18,27 @@ namespace elec.Controllers
             {
                 if (HybridSupport.IsElectronActive)
                 {
+                    ElectronNET.API.Entities.MessageBoxOptions msgOpt = new ElectronNET.API.Entities.MessageBoxOptions("testujem messageboxoptions")
+                    {
+                        Type = ElectronNET.API.Entities.MessageBoxType.question,
+                        Title = "Information",
+                        Buttons = new string[] { "No", "Yes" },
+                        DefaultId = 1,
+                        CancelId = 0
+                    }; 
+                    Electron.AutoUpdater.OnCheckingForUpdate += async () => {
+                         var result = await Electron.Dialog.ShowMessageBoxAsync(msgOpt);
+                        if(result.Response == 1)
+                        {
+                            _logger.Info($"Result response is 1");
+                        }
+                        else
+                        {
+                            _logger.Info($"Result response is 0");
+                        }
+                    };  
                     Electron.AutoUpdater.OnError += (message) => Electron.Dialog.ShowErrorBox("Error", message);
-                    Electron.AutoUpdater.OnCheckingForUpdate += async () => await Electron.Dialog.ShowMessageBoxAsync("Checking for Update");
+                    /* Electron.AutoUpdater.OnCheckingForUpdate += async () => await Electron.Dialog.ShowMessageBoxAsync("Checking for Update"); */
                     Electron.AutoUpdater.OnUpdateNotAvailable += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update not available");
                     Electron.AutoUpdater.OnUpdateAvailable += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update available" + info.Version);
                     Electron.AutoUpdater.OnDownloadProgress += (info) =>
@@ -43,14 +62,14 @@ namespace elec.Controllers
                         var currentVersion = await Electron.App.GetVersionAsync();
                         _logger.Info($"Electron app current version: {currentVersion}");
 
-                        Electron.AutoUpdater.AutoDownload = false;
-                        _logger.Info($"Electron app update - false");
+                        Electron.AutoUpdater.AutoDownload = true;
+                        _logger.Info($"Electron app update - true");
 
-                        #if DEBUG
+                        /* #if DEBUG */
                         var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAsync();
-                        #else
+                        /* #else
                         var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAndNotifyAsync();
-                        #endif
+                        #endif */
 
                         var availableVersion = updateCheckResult.UpdateInfo.Version;
                         _logger.Info($"Available version: {availableVersion}");
@@ -60,6 +79,51 @@ namespace elec.Controllers
 
                         var mainWindow = Electron.WindowManager.BrowserWindows.First();
                         Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
+                    });
+
+                    Electron.IpcMain.On("auto-update-check", async (args) =>
+                    {
+                        // Electron.NET CLI Command for deploy:
+                        // electronize build /target win /electron-params --publish=always
+
+                        var result = await Electron.Dialog.ShowMessageBoxAsync(msgOpt);
+                        if(result.Response == 1)
+                        {
+                            _logger.Info("Result response is 1");
+
+                            var currentVersion = await Electron.App.GetVersionAsync();
+                            _logger.Info($"Electron app current version: {currentVersion}");
+
+                            Electron.AutoUpdater.AutoDownload = false;
+                            _logger.Info("Electron app update - false");
+
+                            /* #if DEBUG */
+                            var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAsync();
+                            /* #else
+                            var updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAndNotifyAsync();
+                            #endif */
+
+                            var availableVersion = updateCheckResult.UpdateInfo.Version;
+                            _logger.Info($"Available version: {availableVersion}");
+
+                            string information = $"Current version: {currentVersion} - available version: {availableVersion}";
+                            _logger.Info(information);
+
+                            var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                            Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
+                        }
+                        else
+                        {
+                            _logger.Info("Result response is 0");
+
+                            string information = "Nechcem aktualizovat";
+                            _logger.Info(information);
+
+                            var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                            Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
+                        }
+                        _logger.Info("End of auto-update-check");
+                        
                     });
                 }
 
